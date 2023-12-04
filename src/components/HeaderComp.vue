26f1289dc84614466d6c5199a9195df9e86fe6f6
Harique <loiza2330@gmail.com>
2023-12-04 14:52:32 +0100

    finished header

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,20 +1,49 @@
 <script lang="ts">
+import '../assets/main.css'
 export default {
-    name:"HeaderComp"
+  name: 'HeaderComp'
 }
 </script>
 
 <template>
-    <header class="header">
-        <h1>Header</h1>
-    </header>
+  <header class="header">
+    <h1>Logo</h1>
+
+    <ul class="nav_bar">
+      <li><a>Resume</a></li>
+      <li><a>Projects</a></li>
+      <li><a>Contact</a></li>
+    </ul>
+
+    <h1>N/D</h1>
+  </header>
 </template>
 
 <style scoped>
-.header{
-    display: flex;
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-shrink: 0;
+  align-self: stretch;
 
-    width: 100%;
-    height: 5rem;
+  height: 5.625rem;
+  color: var(--clr-txt-primary);
+}
+.nav_bar {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
+  align-self: stretch;
+  gap: 3.125rem;
+
+}
+.nav_bar li {
+  display: inline-block;
+
+}
+.nav_bar a{
+    font-size: 18px;
 }
 </style>
