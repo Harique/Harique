655e81b0d73e56619a15bb204d9dd2f4f4bd4eda
Harique <loiza2330@gmail.com>
2023-12-17 00:31:02 +0100

    changed timeline height value and added the years and white line

--- src/components/ResumeComp.vue
+++ src/components/ResumeComp.vue
@@ -9,7 +9,21 @@ export default {
     <h1>Resume</h1>
     <h2>Timeline :</h2>
 
-    <div class="timeline"></div>
+    <div class="timeline">
+      <div class="timeline_work"></div>
+      <div class="white_line"></div>
+      <div class="years">
+        <a>2015</a>
+        <a>2016</a>
+        <a>2017</a>
+        <a>2018</a>
+        <a>2019</a>
+        <a>2020</a>
+        <a>2021</a>
+        <a>2022</a>
+        <a>2023</a>
+      </div>
+    </div>
 
     <h2>Work experience</h2>
 
@@ -99,8 +113,8 @@ export default {
 }
 .timeline {
   display: flex;
-  height: 24.063rem;
-  padding: 0 1.875rem;
+  height: 21rem;
+  padding:  1.875rem;
   flex-direction: column;
   align-items: center;
   gap: 0.625rem;
@@ -126,7 +140,7 @@ export default {
 .skills {
   display: flex;
   flex-direction: column;
-  gap: 0.625rempx;
+  gap: 0.625rem;
   flex: 1 0 0;
 }
 .skillset {
@@ -165,5 +179,33 @@ export default {
   font-style: normal;
   font-weight: 500;
   line-height: normal;
+}
+.timeline_work {
+  display: flex;
+  align-items: center;
+  gap: 0.625rem;
+  flex: 1 0 0;
+  align-self: stretch;
+}
+.white_line {
+  display: flex;
+  width: 100%;
+  height: 5px;
+  background: var(--clr-bg);
+}
+.years {
+  display: flex;
+  height: 1.938rem;
+  justify-content: space-between;
+  align-items: center;
+  flex-shrink: 0;
+  align-self: stretch;
+}
+.years a {
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  color: var(--clr-bg);
 }
 </style>
