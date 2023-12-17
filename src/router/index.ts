61858747867fa5d1a29e9b1213b9e89a5bd8f5c9
Harique <loiza2330@gmail.com>
2023-12-17 11:26:04 +0100

    try 2

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import AppVue from '@/App.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    
+    {path: '/', component: AppVue}
   ]
 })
 
